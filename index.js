02e54a38f8cb9ab126671964198712fa21593e2c
dorothymuhonja <dorothymuhonja7@gmail.com>
2022-06-14 00:59:32 +0300

    commit tree 870a3f9b4213a53e8b149a0c222b14ac6d6e53ff

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function(num1, num2){
+    return num1 / num2
+
+};
+divide(2000, 100)
+
+const square = num => num * num;
+square(2)
+
+const add = (num1, num2) => num1 + num2;
+add(5, 4)
